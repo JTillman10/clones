8d8fe2f99a00960fe8665b081d3f7a9c6c4772bc
Justin Tillman <tillman.55@buckeyemail.osu.edu>
2018-11-06 02:57:28 -0500

    trying with shell scripts

--- scripts/postbuild.sh
+++ scripts/postbuild.sh
@@ -1,1 +1,8 @@
+cd ./ng-trellone
+# echo INSTALLING cli
+# npm install @angular/cli
+echo INSTALL DEPENDENCIES
+npm install
+echo BUILDING FRONTEND FOR PROD
+ng build --aot --prod
 cp -a  ./ng-trellone/dist/. ./dist/client
