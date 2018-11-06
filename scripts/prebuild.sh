5052b1d3aa83a806b060fadcd9da2c4f9c5a0a73
Justin Tillman <tillman.55@buckeyemail.osu.edu>
2018-11-06 02:52:52 -0500

    trying with shell scripts

--- scripts/prebuild.sh
+++ scripts/prebuild.sh
@@ -1,8 +1,8 @@
-cd ./ng-trellone
-echo INSTALLING cli
-npm install @angular/cli
-echo INSTALL DEPENDENCIES
-npm install
-echo BUILDING FRONTEND FOR PROD
-ng build --aot --prod
-ls
+# cd ./ng-trellone
+# echo INSTALLING cli
+# npm install @angular/cli
+# echo INSTALL DEPENDENCIES
+# npm install
+# echo BUILDING FRONTEND FOR PROD
+# ng build --aot --prod
+# ls
